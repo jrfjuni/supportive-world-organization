53b08e58c10921c88bd04622fe42679762fff4c7
Júnior <junnior.ferreira13@hotmail.com>
2023-09-20 20:46:14 -0300

    Create project

---
+++ src/main/java/com/supportiveworld/supportiveworldorganization/SupportiveWorldOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.supportiveworld.supportiveworldorganization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportiveWorldOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportiveWorldOrganizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supportiveworld/supportiveworldorganization/SupportiveWorldOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportiveworld.supportiveworldorganization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportiveWorldOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
